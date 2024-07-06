ba54486e8dbf1cff26d5b4a9ad40c0a8e52012c5
lakindu-banneheka <lakindubannaheka@gmail.com>
2024-07-06 13:03:43 +0530

    fix 1

--- components/nav/header.tsx
+++ components/nav/header.tsx
@@ -85,6 +85,7 @@ export const Header = () => {
               <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
                 { header_links.map((link)=>(
                   <Link
+                    key={link.name}
                     href={link.href}
                     className="text-muted-foreground transition-colors hover:text-foreground"
                   >
