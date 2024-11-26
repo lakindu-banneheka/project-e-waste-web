e558a67dec0851fe1b68823a21d07c53b12d437f
lakindu-banneheka <lakindubannaheka@gmail.com>
2024-11-26 14:36:55 +0530

    bug fix -- fogot password

--- app/(guest_route)/forgot-password/page.tsx
+++ app/(guest_route)/forgot-password/page.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import { z } from "zod";
 import { passwordSchema, universityEmailSchema } from "@/utils/user-validation";
 import ForgotPasswordStepperForm from "@/components/forgot-password/forgot-password-stepper-form";
+import LogoImage from "@/assets/logo/logo";
 
 const FormEmailSchema = z.object({
     email: universityEmailSchema
@@ -27,23 +28,24 @@ const ForgotPassword = () => {
 
     return (
         <>
-            <div className="w-full lg:grid lg:grid-cols-2 h-screen overflow-hidden">
-                <AuthBackground />
-                <div className="flex items-start justify-center py-12">
+            <div className="w-full h-full lg:grid lg:grid-cols-2">
+                <div className="w-full h-full lg:fixed lg:inset-y-0 lg:left-0 lg:w-1/2">
+                    <AuthBackground />
+                </div>
+                <div className="flex items-center justify-center py-12 lg:col-start-2 h-full overflow-y-auto px-10">
                     <div className="mx-auto grid w-[350px] gap-6">
                         <div className="grid gap-2 w-full" >
                             <div className="flex justify-center w-full items-center" >
-                                <Image
-                                    src={logo}
-                                    alt="Image"
-                                    width="200"
-                                    height="80" 
-                                    className="w-auto h-auto"
+                                <LogoImage
+                                    alt="logo"
+                                    // width="200"
+                                    // height="80" 
+                                    className="w-auto max-w-[200px] min-w-[180px] h-auto"
                                 />
                             </div>
                         </div>
                         <div className="grid gap-2 text-center">
-                            <h1 className="text-3xl font-bold">
+                        <h1 className="text-2xl md:text-3xl font-bold">
                                 Reset Password
                             </h1>
                         </div>
